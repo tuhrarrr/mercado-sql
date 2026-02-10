2a2bdab2885126e464f24de5e52bd1d78bdf9e57
tuhrarrr <ruhkkjj@gmail.com>
2026-02-10 19:55:42 -0300

    mercado.sql

--- mercado.sql
+++ mercado.sql
@@ -1,24 +1,24 @@
 -- CRIANDO O BANCO DE DADOS
-CREATE DATABASE mercado;
+CREATE DATABASE IF NOT EXISTS mercado;
 -- USANDO O BANCO DE DADOS
 USE mercado;
 
 -- TABELA DE CLIENTES
-CREATE TABLE clientes(
+CREATE TABLE IF NOT EXISTS clientes(
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome_cliente VARCHAR(100) NOT NULL,
     email_cliente VARCHAR(100) UNIQUE
 );
 
 -- TABELA DE PRODUTOS
-CREATE TABLE produtos(
+CREATE TABLE IF NOT EXISTS produtos(
     id_produto INT AUTO_INCREMENT PRIMARY KEY,
     nome_produto VARCHAR(100) NOT NULL,
     preco DECIMAL(10, 2)
 );
 
 -- TABELA DE VENDA
-CREATE TABLE venda(
+CREATE TABLE IF NOT EXISTS venda(
     id_venda INT AUTO_INCREMENT PRIMARY KEY,
     cliente_comprou int not null,
     data_venda DATE NULL,
@@ -27,7 +27,7 @@ CREATE TABLE venda(
 
 
 -- TABELA DE ITEM VENDA
-CREATE TABLE item_venda(
+CREATE TABLE IF NOT EXISTS item_venda(
     id_item INT AUTO_INCREMENT PRIMARY KEY,
     id_item_venda int,
     id_item_produto int,
@@ -38,12 +38,12 @@ CREATE TABLE item_venda(
 );
 
 -- Inserindo Dados
-INSERT INTO clientes (nome_cliente, email_cliente) VALUES ('Paulin', 'paulinbacana123faavela@gmail.com'), ('Jorge', 'jorgequebrada@gmail.com'), 
+INSERT IGNORE INTO clientes (nome_cliente, email_cliente) VALUES ('Paulin', 'paulinbacana123faavela@gmail.com'), ('Jorge', 'jorgequebrada@gmail.com'), 
                      ('João', 'jaozingameplay@gmail.com'), ('Arthur', 'tutuproducoes@gmail.com'), ('tyler', 'tylerthecreator@gmail.com');
-INSERT INTO produtos (nome_produto, preco) VALUES ('Teclado', '120.00'), ('Mouse', '90.00'), ('Fone', '60.00');
-INSERT INTO venda (cliente_comprou, data_venda) VALUES (1, '2026-02-10'), (2, '2026-02-11'), (3, '2036-02-12'), (4, '2040-12-12'),
+INSERT IGNORE INTO produtos (nome_produto, preco) VALUES ('Teclado', '120.00'), ('Mouse', '90.00'), ('Fone', '60.00');
+INSERT IGNORE INTO venda (cliente_comprou, data_venda) VALUES (1, '2026-02-10'), (2, '2026-02-11'), (3, '2036-02-12'), (4, '2040-12-12'),
                   (5, '2080-12-31');
-INSERT INTO item_venda (id_item_venda, id_item_produto, qtd, preco_unit) VALUES (1, 1, 2, '120.00'), (2, 2, 1, '90.00'), (3, 3, 2, '60.00'), 
+INSERT IGNORE INTO item_venda (id_item_venda, id_item_produto, qtd, preco_unit) VALUES (1, 1, 2, '120.00'), (2, 2, 1, '90.00'), (3, 3, 2, '60.00'), 
                        (4, 2, 10, '90.00'), (5, 1, 2300, '120.00');
 
 -- TESTES....
